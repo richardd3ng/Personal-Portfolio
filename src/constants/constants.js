90298d6abecac18539688ffa090723197a6b3155
Richard Deng <richarddeng@Richards-MacBook-Pro.local>
2023-05-18 22:13:24 -0700

    updated project descriptions

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const ProfileDescription = (
   <>
-    I'm a passionate software developer interested in Full Stack Mobile & Web
-    Development and Artificial Intelligence/Machine Learning
+    I am a self-driven software developer interested in Full Stack Mobile & Web
+    Development and Artificial Intelligence/Machine Learning!
   </>
 );
 
@@ -10,15 +10,15 @@ export const projects = [
     title: "Image Reconstruction",
     description: (
       <>
-        This Image Reconstruction project explores the process of recovering
-        digital images that contain corrupted pixels using statistical learning
-        approaches such as regression and regularization. The general approach
-        taken involved dividing the corrupted image into blocks, reconstructing
-        each individual block, and concatenating all reconstructed blocks into a
-        full image. The reconstruction algorithm involves fitting a
-        Lasso-regularized model with Discrete Cosine Transform (DCT) basis
-        vectors as features. My full project presentation slidedoc is linked
-        below in "More Info".
+        This project explores the process of recovering digital images that
+        contain corrupted pixels using statistical learning approaches such as
+        regression and regularization. The general procedure involved dividing
+        the corrupted image into blocks, reconstructing each individual block,
+        and concatenating all reconstructed blocks into a full image. The
+        reconstruction algorithm revolves around fitting a Lasso-regularized
+        model with Discrete Cosine Transform (DCT) basis vectors as features. To
+        learn more, my full presentation slidedoc is linked below in "More
+        Info."
       </>
     ),
     image: "images/image_reconstruction.png",
@@ -36,11 +36,11 @@ export const projects = [
         distinguish between left and right-handed movement using brain surface
         electrode signals as features. Brain Computer Interfaces (BCI's) are a
         relatively new technology that presents significant potential to help
-        individuals who lose the ability to perform normal motor functions. If
-        neural signals can be accurately translated into true movement intents,
-        BCI's can enable individuals to perform physical movements by simply
-        imagining them. My full project presentation slidedoc is linked below in
-        "More Info".
+        individuals with nerve damage who lose the ability to perform normal
+        motor functions. If neural signals can be accurately translated into
+        real movement intents, BCI's can enable individuals to perform physical
+        movements by simply imagining them. To learn more, my full presentation
+        slidedoc is linked below in "More Info."
       </>
     ),
     image: "images/bci.png",
@@ -65,7 +65,7 @@ export const projects = [
         TestFlight designed to help medical professionals collect handwriting
         data from patients for neurodegenerative disease diagnosis purposes. The
         full paper detailing the ML analysis pipeline we developed is linked
-        below in "More Info".
+        below in "More Info."
       </>
     ),
     image: "images/NScribe_logo.png",
@@ -78,10 +78,16 @@ export const projects = [
     title: "MusicMap",
     description: (
       <>
-        This is a class project I worked on in a team of 6. MusicMap is a mobile
-        app that documents the songs played during road trips, allowing users to
-        not only revisit a catalog of all the songs played during the trip, but
-        also the specific locations associated with each song.
+        MusicMap, a class project I worked on in a team of 6, is a mobile app
+        that documents the songs played during road trips. The app allows users
+        to revisit a catalog of all the songs played during the trip and see the
+        specific locations associated with each song. A map screen displays the
+        user’s current location and drops pins during song changes or image
+        uploads. Users can also relive their memories by viewing video collages
+        of their songs and images during past trips. I primarily worked on
+        implementing the map screen visuals and functionality, which involved
+        building various UI components and performing API calls to retrieve
+        location data and detect Spotify song changes.
       </>
     ),
     image: "images/MusicMap.png",
